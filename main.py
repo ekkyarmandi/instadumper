8b6fbe2ebc6a3642977bf1f67516e2365cb81e64
Ekky Armandi <ekkyarmandi@gmail.com>
2022-05-25 19:45:27 +0700

    refine the code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from instaloader import Instaloader, Profile
 from credentials import USERNAME, PASSWORD
 from pprint import pprint
+from pandas import DataFrame
 import json
 
 # login into Instagram
@@ -12,36 +13,44 @@ target_handle = "worldofnolabel"
 profile = Profile.from_username(L.context,target_handle)
 
 # iterate account followers
-entries = []
-for user in profile.get_followers():
+try:
+    entries = []
+    for user in profile.get_followers():
 
-    # find latest post
-    for post in user.get_posts():
-        post_url = post.url
-        post_date = post.date_local.strftime("%H:%M:%S %d/%m/%Y")
-        break
+        # find latest post
+        for post in user.get_posts():
+            post_url = "https://www.instagram.com/p/" + str(post.shortcode)
+            post_date = post.date_local.strftime("%H:%M:%S %d/%m/%Y")
+            break
 
-    # define entry data
-    entry = dict( 
-        id=user.userid,
-        username=user.username,
-        fullname=user.full_name,
-        followers=user.followers,
-        followings=user.followees,
-        posts=user.mediacount,
-        last_post_url=post_url,
-        last_post_date=post_date,
-        biography=user.biography,
-        is_bussiness=user.is_business_account,
-        business_category=user.business_category_name,
-        links=user.external_url
-    )
-    entries.append(entry)
-    pprint(entry, sort_dicts=False)
+        # define entry data
+        entry = dict( 
+            id=user.userid,
+            username=user.username,
+            fullname=user.full_name,
+            followers=user.followers,
+            followings=user.followees,
+            posts=user.mediacount,
+            last_post_url=post_url if post.mediacount != 0 else None,
+            last_post_date=post_date if post.mediacount != 0 else None,
+            biography=user.biography,
+            is_bussiness=user.is_business_account,
+            business_category=user.business_category_name,
+            links=user.external_url
+        )
+        entries.append(entry)
+        pprint(entry, sort_dicts=False)
+
+except Exception:
+    print(Exception)
 
 # dump the collected data
 json.dump(
     entries,
-    open("entries.json","w",encoding="utf-8"),
+    open(target_handle+".json","w",encoding="utf-8"),
     indent=4
 )
+
+# write it out into a csv
+data = DataFrame(entries)
+data.to_csv(target_handle+".csv",index=False)
